907a09d65849041c4731c2e8c168b4ef9a01c857
Leonardo <leonardodamata@gmail.com>
2013-06-27 10:58:01 -0300

    TESTE NO GIT

--- src/br/com/especializacao/sevira/MainActivity.java
+++ src/br/com/especializacao/sevira/MainActivity.java
@@ -14,8 +14,13 @@ public class MainActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
+		
+	    // TESTE NO GIT	
 		// Inflate the menu; this adds items to the action bar if it is present.
+		
 		getMenuInflater().inflate(R.menu.main, menu);
+		
+		
 		return true;
 	}
 
